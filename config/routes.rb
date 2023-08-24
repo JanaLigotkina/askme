4859101f7b12ad52357c92fdbf1f70a9b1e4db4a
jana <janakolesnikova6293@gmail.com>
2023-08-25 01:34:51 +0400

    added authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'questions#index' 
+
   resources :questions do 
     put :hide, on: :member
     patch :hide, on: :member
-  end  
+  end 
+
+  resource :session, only: %i[new create destroy]
+  resources :users, only: %i[new create] 
 end
